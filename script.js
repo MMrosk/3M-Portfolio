a3ab30b73b847f638d99311154c157772ee501b1
MMrosk <Mrosk.Maximilian@outlook.de>
2025-05-29 12:53:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function HellDunkel(){
+
+        var hell = document.getElementById("hell")
+
+        
+
+
+
+       
+        hell.style.backgroundColor = "rgb(4, 7, 60)"
+        
+
+    }
+
+
+document.getElementById("button").addEventListener('click', () => {
+    HellDunkel()
+
+});
